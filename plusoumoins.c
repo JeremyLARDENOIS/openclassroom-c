4add7cf7c43eff3d736a6ddabd8e7b28fd8c509a
repl.it user <replituser@example.com>
2020-08-08 10:27:13 +0000

    Rejouer

--- plusoumoins.c
+++ plusoumoins.c
@@ -3,26 +3,40 @@
 #include <time.h>
 
 int main(void) {
-  int choix, nombreMystere;
+  int choix, nombreMystere, essai;
   const int MIN = 0, MAX = 100;
+  int nouvellepartie = 1; //boolean 
 
   srand(time(NULL));
-  nombreMystere = (rand() % (MAX - MIN + 1)) + MIN;
-
-  printf("Le but du jeu est de deviner le nombre mystère entre 0 et 100\n");
   
-  choix = -1;
-  while(choix != nombreMystere){
-    printf("Quel est le nombre mystère ?\n-> ");
-    scanf("%d",&choix);
+  while (nouvellepartie){
+    essai = 0;
+    nombreMystere = (rand() % (MAX - MIN + 1)) + MIN;
+  
+    printf("Le but du jeu est de deviner le nombre mystère entre 0 et 100\n\n");
+    
+    choix = -1;
+    while(choix != nombreMystere){
+      printf("Quel est le nombre mystère ?\n-> ");
+      scanf("%d",&choix);
+      essai++;
 
-    if  (choix < nombreMystere){
-      printf("C'est + !\n\n");
+      if  (choix < nombreMystere){
+        printf("C'est + !\n\n");
+      }
+      else if (choix > nombreMystere){
+        printf("C'est - !\n\n");
+      }
     }
-    else if (choix > nombreMystere){
-      printf("C'est - !\n\n");
+    printf("Vous avez trouvé en %d essai(s) !\n", essai); 
+
+    printf("\nVoulez vous rejouer ?\n1 : Oui\n2 : Non\n-> ");
+    scanf("%d",&choix);
+    if (choix != 1){
+      nouvellepartie = 0;
+    } else {
+      printf("################\n");
     }
   }
-  printf("Vous avez trouvé !\n"); 
   return 0;
 }
