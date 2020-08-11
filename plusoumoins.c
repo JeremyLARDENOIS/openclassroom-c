9af7de34256fd1cf9ff3da74dc57344245a8d2b6
Jérémy LARDENOIS <37746231+JeremyLARDENOIS@users.noreply.github.com>
2020-08-11 15:58:29 +0200

    Update plusoumoins.c
    Rejouer + 2 joueurs

--- plusoumoins.c
+++ plusoumoins.c
@@ -3,26 +3,51 @@
 #include <time.h>
 
 int main(void) {
-  int choix, nombreMystere;
+  int choix, nombreMystere, essai, nombreJoueurs = 1;
   const int MIN = 0, MAX = 100;
+  int nouvellepartie = 1; //boolean 
 
   srand(time(NULL));
-  nombreMystere = (rand() % (MAX - MIN + 1)) + MIN;
-
-  printf("Le but du jeu est de deviner le nombre mystère entre 0 et 100\n");
   
-  choix = -1;
-  while(choix != nombreMystere){
-    printf("Quel est le nombre mystère ?\n-> ");
-    scanf("%d",&choix);
+  while (nouvellepartie){
+    essai = 0;
+    
+    printf("Le but du jeu est de deviner le nombre mystère entre 0 et 100\n\n");
 
-    if  (choix < nombreMystere){
-      printf("C'est + !\n\n");
+    printf ("Combien de joueurs participeront ?\n1 : 1 joueur\n2 : 2 joeuurs\n-> ");
+    scanf("%d",&nombreJoueurs);
+
+    if (nombreJoueurs == 1) {
+      nombreMystere = (rand() % (MAX - MIN + 1)) + MIN;
     }
-    else if (choix > nombreMystere){
-      printf("C'est - !\n\n");
+    else if (nombreJoueurs == 2) {
+      printf("Le premier joueur choisi le nombre mystère et le deuxième essaie de le deviner. Le nombre mystère doit se situer entre 0 et 100. Quel sera le nombre mystère ?\n-> ");
+      scanf("%d",&nombreMystere);
+      system("clear");
+    }
+   
+    choix = -1;
+    while(choix != nombreMystere){
+      printf("Quel est le nombre mystère ?\n-> ");
+      scanf("%d",&choix);
+      essai++;
+
+      if  (choix < nombreMystere){
+        printf("C'est + !\n\n");
+      }
+      else if (choix > nombreMystere){
+        printf("C'est - !\n\n");
+      }
+    }
+    printf("Vous avez trouvé en %d essai(s) !\n", essai); 
+
+    printf("\nVoulez vous rejouer ?\n1 : Oui\n2 : Non\n-> ");
+    scanf("%d",&choix);
+    if (choix != 1){
+      nouvellepartie = 0;
+    } else {
+      printf("################\n");
     }
   }
-  printf("Vous avez trouvé !\n"); 
   return 0;
 }
