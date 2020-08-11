ac061fa1f03ab4bf4a34f3cb0d6c5b81440d9997
JeremyLARDENOIS <jeremy.lardenois@hotmail.fr>
2020-08-11 14:52:15 +0100

    Revert "Rejouer"
    This reverts commit 4add7cf7c43eff3d736a6ddabd8e7b28fd8c509a.

--- plusoumoins.c
+++ plusoumoins.c
@@ -3,40 +3,26 @@
 #include <time.h>
 
 int main(void) {
-  int choix, nombreMystere, essai;
+  int choix, nombreMystere;
   const int MIN = 0, MAX = 100;
-  int nouvellepartie = 1; //boolean 
 
   srand(time(NULL));
-  
-  while (nouvellepartie){
-    essai = 0;
-    nombreMystere = (rand() % (MAX - MIN + 1)) + MIN;
-  
-    printf("Le but du jeu est de deviner le nombre mystère entre 0 et 100\n\n");
-    
-    choix = -1;
-    while(choix != nombreMystere){
-      printf("Quel est le nombre mystère ?\n-> ");
-      scanf("%d",&choix);
-      essai++;
+  nombreMystere = (rand() % (MAX - MIN + 1)) + MIN;
 
-      if  (choix < nombreMystere){
-        printf("C'est + !\n\n");
-      }
-      else if (choix > nombreMystere){
-        printf("C'est - !\n\n");
-      }
-    }
-    printf("Vous avez trouvé en %d essai(s) !\n", essai); 
-
-    printf("\nVoulez vous rejouer ?\n1 : Oui\n2 : Non\n-> ");
+  printf("Le but du jeu est de deviner le nombre mystère entre 0 et 100\n");
+  
+  choix = -1;
+  while(choix != nombreMystere){
+    printf("Quel est le nombre mystère ?\n-> ");
     scanf("%d",&choix);
-    if (choix != 1){
-      nouvellepartie = 0;
-    } else {
-      printf("################\n");
+
+    if  (choix < nombreMystere){
+      printf("C'est + !\n\n");
+    }
+    else if (choix > nombreMystere){
+      printf("C'est - !\n\n");
     }
   }
+  printf("Vous avez trouvé !\n"); 
   return 0;
 }
